efccaaad215abcf9185fb2e7aa3fb7e356b2661e
lishiyun19 <lishiyun19@gmail.com>
2018-08-11 17:21:44 +0800

    update advance search

---
+++ Ad_Splunk.py
@@ -0,0 +1,57 @@
+class Ad_Splunk(object):
+    def __init__(self):
+        self.bf = Bloomfilter(64)
+        self.terms = {}  # Dictionary of term to set of events
+        self.events = []
+    
+    def add_event(self, event):
+        """Adds an event to this object"""
+
+        # Generate a unique ID for the event, and save it
+        event_id = len(self.events)
+        self.events.append(event)
+
+        # Add each term to the bloomfilter, and track the event by each term
+        for term in segments(event):
+            self.bf.add_value(term)
+            if term not in self.terms:
+                self.terms[term] = set()
+            
+            self.terms[term].add(event_id)
+
+    def search_all(self, terms):
+        """Search for an AND of all terms"""
+
+        # Start with the universe of all events...
+        results = set(range(len(self.events)))
+
+        for term in terms:
+            # If a term isn't present at all then we can stop looking
+            if not self.bf.might_contain(term):
+                return
+            if term not in self.terms:
+                return
+
+            # Drop events that don't match from our results
+            results = results.intersection(self.terms[term])
+
+        for event_id in sorted(results):
+            yield self.events[event_id]
+
+
+    def search_any(self, terms):
+        """Search for an OR of all terms"""
+        results = set()
+
+        for term in terms:
+            # If a term isn't present, we skip it, but don't stop
+            if not self.bf.might_contain(term):
+                continue
+            if term not in self.terms:
+                continue
+
+            # Add these events to our results
+            results = results.union(self.terms[term])
+
+        for event_id in sorted(results):
+            yield self.events[event_id]
